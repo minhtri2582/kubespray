998a25c3dabd0ded3a6e2125462cdc8700e779ca
Tri Do Le Minh <minhtri2582@gmail.com>
2023-03-02 06:21:33 +0700

    modified run.sh in kubespray

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker run --rm -it --mount type=bind,source=/tmp/kubespray/elisoft-cluster,dst=
   --mount type=bind,source=/tmp/.ssh/id_rsa,dst=/home/sysadmin/.ssh/id_rsa \
   quay.io/kubespray/kubespray:v2.16.0 bash
 
-ansible-playbook -i /inventory/hosts.yaml cluster.yml --user=sysadmin --ask-pass --become --ask-become-pass 
+ansible-playbook -i /inventory/hosts.ini cluster.yml --user=sysadmin --ask-pass --become --ask-become-pass 
